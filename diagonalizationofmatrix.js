5198002ac065e622d0130d8c81394df7c522e6d9
Subash Praveen <subash23050214@gmail.com>
2023-11-21 20:32:27 +0530

    Added  -matrix input fields  -title  -calculate button  -output box (console) 	deleted:    diagonalization-of-matrix.js 	new file:   diagonalizationofmatrix.js 	modified:   index.html 	modified:   style.css

---
+++ diagonalizationofmatrix.js
@@ -0,0 +1,16 @@
+class diagonalizationofmatrix {
+    constructor() {
+        this.matrix = [];
+
+        for (var i = 0; i < 3; i++) {
+            this.matrix[i] = [];
+        }
+
+        this.xdimension = 3;
+        this.ydimension = 3;
+    }
+
+    calculate() {
+        
+    }
+}
