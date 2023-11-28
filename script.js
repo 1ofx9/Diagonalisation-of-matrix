5f596237a720be63730c51243058138fad7ea4f3
Subash Praveen <subash23050214@gmail.com>
2023-11-28 23:57:26 +0530

    major changes
    added math.js (math lib for js)
    added a way to calculate display eigen values.
    added a way to calculate determinant of the matrix
    
    other minor changes
    made the code neat. (i tied :c)

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Author: @Subash Praveen Instagram: @sub._.praveen Github: @SUBASH2309
 
+// visibility of cells 3x3
 function three() {
     document.getElementById('threer1c1').style.visibility = "visible";    
     document.getElementById('threer1c2').style.visibility = "visible";    
@@ -23,6 +24,7 @@ function three() {
     document.getElementById('end').style.visibility = "hidden";
 }
 
+// visibility of cells 2x2
 function two() {
     document.getElementById('twor1c1').style.visibility = "visible";
     document.getElementById('twor1c2').style.visibility = "visible";
@@ -47,9 +49,44 @@ function two() {
 }
 
 function calculate2() {
-    document.getElementById('textarea').value = "HELLO, WoRLD for cal2";
+    // output box
+    const twoutbox = document.getElementById('textarea');
+    // row1
+    var twr1c1 = document.getElementById("twor1c1").value;
+    var twr1c2 = document.getElementById("twor1c2").value;
+    // row2
+    var twr2c1 = document.getElementById("twor2c1").value;
+    var twr2c2 = document.getElementById("twor2c2").value;
+
+    // to find Determinant of the matrix
+    var det;
+    det = (twr1c1*twr2c2)-(twr1c2*twr2c1)
+
+    // to get eigen values
+    var twev;
+    twev = math.eigs([[twr1c1, twr1c2], [twr2c1, twr2c2]], {eigenvectors: false}); //outputs dict containing only eigenvalues as 'values'
+
+    // to display output
+    twoutput = "Eigen values ('λ') = "+math.round(Object.values(twev))+"\nEigen Vectors = "; //this only displays eigen values
+    twoutbox.innerHTML = twoutput;
 }
 
 function calculate3() {
-    document.getElementById('textarea').value = "HELLO, WoRLD for cal3";
+    // output box
+    const outbox = document.getElementById('textarea');
+    // row1
+    var thr1c1 = document.getElementById("threer1c1").value;
+    var thr1c2 = document.getElementById("threer1c2").value;
+    var thr1c3 = document.getElementById("threer1c3").value;
+    // row2
+    var thr2c1 = document.getElementById("threer2c1").value;
+    var thr2c2 = document.getElementById("threer2c2").value;
+    var thr2c3 = document.getElementById("threer2c3").value;
+    // row3
+    var thr3c1 = document.getElementById("threer3c1").value;
+    var thr3c2 = document.getElementById("threer3c2").value;
+    var thr3c3 = document.getElementById("threer3c3").value;
+
+
+
 }
